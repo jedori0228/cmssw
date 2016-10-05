df5316a6c7ad2dbfbb11e73152752fac7f694f3d
Jae Sung Kim <jae.sung.kim@cern.ch>
2016-10-06 01:24:56 +0900

    remove useless file

--- RecoMuon/MuonIdentification/test/trackerGEM_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-trackerGEM = cms.EDProducer("trackerGEM",
-    gemSegmentsToken = cms.InputTag("gemSegments"),
-    generalTracksToken = cms.InputTag("generalTracks"),
-)

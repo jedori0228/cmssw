2ac22dbd5e01f5b9a550cb3a26ab7d0d3c6e6fa6
Jae Sung Kim <jae.sung.kim@cern.ch>
2019-01-01 19:37:41 +0100

    fix default value

--- L1Prefiring/EventWeightProducer/python/L1ECALPrefiringWeightProducer_cfi.py
+++ L1Prefiring/EventWeightProducer/python/L1ECALPrefiringWeightProducer_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 prefiringweight = cms.EDProducer("L1ECALPrefiringWeightProducer",
                                  ThePhotons = cms.InputTag("slimmedPhotons"),
                                  TheJets = cms.InputTag("slimmedJets"),
-                                 L1Maps = cms.FileInPath("L1Prefiring/EventWeightProducer/files/L1PrefiringMaps_new.root"),
+                                 L1Maps = cms.FileInPath("SKFlatMaker/SKFlatMaker/data/L1Prefire/L1PrefiringMaps_new.root"),
                                  DataEra = cms.string("2017BtoF"),
                                  UseJetEMPt = cms.bool(True),
                                  PrefiringRateSystematicUncty = cms.double(0.2)
